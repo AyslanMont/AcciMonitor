2089f265750afa1ca67ba2c709d9254a48aaac23
Ayslan Francisco Monteiro Neto <ayslan.monteiro@escolar.ifrn.edu.br>
2023-11-18 14:39:05 -0300

    Estruturando, e construidos os primeiros widgets

--- App.py
+++ App.py
@@ -6,9 +6,32 @@ class App:
     def __init__(self):
         self.janela = Tk()
         self.janela.title("AcciMonitor")
-        self.janela.geometry("800x800")
+        self.janela.iconbitmap("assets/imgs/acidente.ico")
+        self.janela.geometry("800x500")
+
+        self.Canvas_background = Canvas(self.janela, background="Gray")
+        self.Canvas_background.pack(fill="both", expand=True)
+
+        #Menu Principal
+        self.Menu_Principal = Menu(self.janela)
+        self.Menu_Principal.add_command(label="Cadastro", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Cadastro()))
+        self.Menu_Principal.add_command(label="Consulta", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Consulta()))
+
+        self.janela.config(menu=self.Menu_Principal)
 
         self.janela.mainloop()
+
+    def Exibir_Aba_Cadastro(self):
+        pass
+        
+    def Exibir_Aba_Consulta(self):
+        pass
+
+    def Limpar_Tela(self):
+        for widget in self.janela.winfo_children():
+            widget.destroy()
+
+
 
 if __name__ == "__main__":
     EXECUCAO = App()
