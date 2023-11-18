f998d4c4d6d57a5fcde8a68c5bf00c0b7ac0bd01
Ayslan Francisco Monteiro Neto <ayslan.monteiro@escolar.ifrn.edu.br>
2023-11-18 15:53:19 -0300

    Definindo os primeiros widgets

--- App.py
+++ App.py
@@ -1,13 +1,45 @@
 from tkinter import *
+from tkinter.ttk import *
 import sqlite3
 import pickle
+import json
 
 class App:
     def __init__(self):
         self.janela = Tk()
         self.janela.title("AcciMonitor")
         self.janela.iconbitmap("assets/imgs/acidente.ico")
-        self.janela.geometry("800x500")
+        self.janela.geometry("800x600")
+
+        #Cores 
+
+        self.Background_da_Janela = "#E0E0E0"
+        self.Azul_Primario = "#2196F3" 
+        self.Azul_Secundario = "#03A9F4" 
+        self.Verde_Sucesso = "#4CAF50" 
+        self.Vermelho_Aviso = "#FF5722" 
+        self.Amarelo_Alerta = "#FFC107" 
+        
+
+        self.Canvas_background = Canvas(self.janela, background=self.Background_da_Janela)
+        self.Canvas_background.pack(fill="both", expand=True)
+
+        #Menu Principal
+        self.Menu_Principal = Menu(self.janela)
+        self.Menu_Principal.add_command(label="Cadastro", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Cadastro()))
+        self.Menu_Principal.add_command(label="Consulta", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Consulta()))
+
+        self.janela.config(menu=self.Menu_Principal)
+
+        #Informações
+
+        self.Label_Titulo_Informacoes = Label(self.janela, text="Informações", background=self.Azul_Primario, font="Arial 20", width=30,anchor=CENTER)
+        self.Label_Titulo_Informacoes.place(x=50, y=10)
+
+
+        self.janela.mainloop()
+
+    def Exibir_Aba_Cadastro(self):
 
         self.Canvas_background = Canvas(self.janela, background="Gray")
         self.Canvas_background.pack(fill="both", expand=True)
@@ -18,14 +50,18 @@ class App:
         self.Menu_Principal.add_command(label="Consulta", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Consulta()))
 
         self.janela.config(menu=self.Menu_Principal)
-
-        self.janela.mainloop()
-
-    def Exibir_Aba_Cadastro(self):
-        pass
         
     def Exibir_Aba_Consulta(self):
-        pass
+
+        self.Canvas_background = Canvas(self.janela, background="Gray")
+        self.Canvas_background.pack(fill="both", expand=True)
+
+        #Menu Principal
+        self.Menu_Principal = Menu(self.janela)
+        self.Menu_Principal.add_command(label="Cadastro", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Cadastro()))
+        self.Menu_Principal.add_command(label="Consulta", command=lambda: (self.Limpar_Tela(),self.Exibir_Aba_Consulta()))
+
+        self.janela.config(menu=self.Menu_Principal)
 
     def Limpar_Tela(self):
         for widget in self.janela.winfo_children():
