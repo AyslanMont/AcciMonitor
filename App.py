9f3f8124a57cc2445c6187e1a0a190d76125f85c
Ayslan Francisco Monteiro Neto <ayslan.monteiro@escolar.ifrn.edu.br>
2023-11-18 21:53:22 -0300

    Construindo a interface na parte de informações

--- App.py
+++ App.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import filedialog
 import sqlite3
 import pickle
 import json
+from PIL import ImageTk, Image
 
 class App:
     def __init__(self):
@@ -33,8 +35,51 @@ class App:
 
         #Informações
 
-        self.Label_Titulo_Informacoes = Label(self.janela, text="Informações", background=self.Azul_Primario, font="Arial 20", width=30,anchor=CENTER)
-        self.Label_Titulo_Informacoes.place(x=50, y=10)
+        self.Label_Titulo_Informacoes = Label(self.janela, text="Informações", background=self.Azul_Primario, font="Arial 20",anchor=CENTER)
+        self.Label_Titulo_Informacoes.place(x=50, y=10, width=500)
+
+        #Informações -- Data
+
+        self.Label_Data = Label(self.janela, text="Data", background=self.Amarelo_Alerta, font="Arial 15",anchor=CENTER)
+        self.Label_Data.place(x=50, y=70, width=60, height=25)
+        self.Entry_data = Entry(self.janela)
+        self.Entry_data.place(x=120, y=70, width=175, height=25)
+        
+        #Informações -- Hora
+
+        self.Label_Hora = Label(self.janela, text="Hora", background=self.Amarelo_Alerta, font="Arial 15",anchor=CENTER)
+        self.Label_Hora.place(x=305, y=70, width=60, height=25)
+        self.Entry_Hora = Entry(self.janela)
+        self.Entry_Hora.place(x=375, y=70, width=175, height=25)
+
+        #Informações -- Local
+
+        self.Label_Local = Label(self.janela,text="Local",background=self.Amarelo_Alerta, font="Arial 15",anchor=CENTER)
+        self.Label_Local.place(x=50, y=115, width=60, height=25)
+        self.Entry_Local = Entry(self.janela)
+        self.Entry_Local.place(x=120, y=115, width=430, height=25)
+
+
+        #Informações -- Categoria
+
+        self.Opcoes_de_Categoria = ["Colisões","Queda", "Incêndio", "Explosão", "Outro"]
+        self.Label_Gravidade = Label(self.janela,text="Gravidade",background=self.Amarelo_Alerta, font="Arial 15",anchor=CENTER)
+        self.Label_Gravidade.place(x=50, y=160,width=200, height=25)
+        self.Entry_Gravidade = Combobox(self.janela, values=self.Opcoes_de_Categoria, font="Arial 10")
+        self.Entry_Gravidade.place(x=50, y=195,width=200, height=25)
+
+        #Informações -- Descrição
+        self.Label_Descricao = Label(self.janela,text="Descrição",background=self.Amarelo_Alerta, font="Arial 15",anchor=CENTER)
+        self.Label_Descricao.place(x=260, y=160,width=290, height=25)
+        self.Entry_Descricao = Text(self.janela)
+        self.Entry_Descricao.place(x=260, y=195,width=290, height=200)
+
+        #Informações -- Fotos
+
+        self.Label_Imagem = Label(self.janela)
+        self.Label_Imagem.place(x=50, y=400)
+        self.botao_Anexar_Imagem = Button(self.janela, text="Anexar Imagem", command=self.Selecionar_Imagem)
+        self.botao_Anexar_Imagem.place(x=500, y=400)
 
 
         self.janela.mainloop()
@@ -66,6 +111,15 @@ class App:
     def Limpar_Tela(self):
         for widget in self.janela.winfo_children():
             widget.destroy()
+
+    def Selecionar_Imagem(self):
+        arquivo = filedialog.askopenfilename(filetypes=[("Imagens", "*.png;*.jpg;*.jpeg")])
+        if arquivo:
+            Imagem = Image.open(arquivo)
+            #Imagem = Image.thumbnail((400,300))
+            photo = ImageTk.PhotoImage(Imagem)
+            self.Label_Imagem.config(image=photo)
+            self.Label_Imagem.image = photo
 
 
 
